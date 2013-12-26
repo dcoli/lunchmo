e18cb92fe8166dff2047ddaa3229d408635fbf98
vagrant <vagrant@rails-starter-box>
2013-12-26 19:48:26 +0000

    These weren't being saved to git before, let's see if it works after I explicitly added edit.html.erb.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,28 @@
 class JobsController < ApplicationController
+ 
   def index
+  	@jobs = Job.find_all( { order_by: "updated_at DESC" } )
   end
 
-  def hello
+  def show
+  	@job = Job.find( params[:id] )
   end
+
+  def edit
+  	@job = Job.find( params[:id] )
+  end
+
+  def update
+  	@job = Job.update( params )
+  end
+
+  def new
+  	@job = Job.new
+  end
+
+  def create
+  	#need to check for errors
+  	@job = Job.create( params )
+  end
+
 end
